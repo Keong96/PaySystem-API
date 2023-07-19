15ce978ce7a1e5e55f63c6f5be5f85acca019190
Yamei-Github <headendekay2@outlook.com>
2023-07-19 16:26:09 +0800

    a

--- index.js
+++ index.js
@@ -131,6 +131,7 @@ app.get('/home/get', verifyToken, async (req, res) => {
             }
           }
 
+          data = JSON.parse(JSON.stringify(data));
           data['income'] = income.slice(0, 5);
           data['expense'] = expense.slice(0, 5);
           data['deposit'] = 100;
