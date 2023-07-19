8fb6df8d535593b0acab5e8eada4e73345e6b0c6
Yamei-Github <headendekay2@outlook.com>
2023-07-19 15:01:23 +0800

    a

--- index.js
+++ index.js
@@ -114,36 +114,36 @@ app.get('/home/get', verifyToken, async (req, res) => {
     var data = [];
     data['income'] = [];
     data['expense'] = [];
-    
+
     client.query("SELECT * FROM requests ORDER BY datetime")
           .then((result) => {
-
-          var income = [];
-          var expense = [];
+          res.send(JSON.stringify(result.rows));
+          // var income = [];
+          // var expense = [];
           
-          for(var i = 0; i < result.rows.length; i++)
-          {
-            if(result.rows[i].request_type == 0)
-            {
-              income.push(result.rows[i]);
-            }
-            else
-            {
-              expense.push(result.rows[i]);
-            }
-          }
+          // for(var i = 0; i < result.rows.length; i++)
+          // {
+          //   if(result.rows[i].request_type == 0)
+          //   {
+          //     income.push(result.rows[i]);
+          //   }
+          //   else
+          //   {
+          //     expense.push(result.rows[i]);
+          //   }
+          // }
 
-          data['income'] = income.slice(0, 5);
-          data['expense'] = expense.slice(0, 5);
+          // data['income'] = income.slice(0, 5);
+          // data['expense'] = expense.slice(0, 5);
 
-          console.log("data = "+data);
-          res.send(JSON.stringify(data));
+          // console.log("data = "+data);
+          // res.send(JSON.stringify(data));
 
-          })
-          .catch((e) => {
-            console.error(e.stack);
-            res.status(500).send(e.stack);
-          })
+          // })
+          // .catch((e) => {
+          //   console.error(e.stack);
+          //   res.status(500).send(e.stack);
+          // })
   }
   else
   {
