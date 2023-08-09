68087f9470a7c10b33fd4266f62ee9edc63f6270
Yamei-Github <headendekay2@outlook.com>
2023-08-09 20:24:36 +0800

    a

--- index.js
+++ index.js
@@ -511,34 +511,23 @@ app.post('/getCoin', async (req, res) => {
                                         con.query(sql, function (err, result4) {
                                           if (err) throw err;
 
-                                          res.send(JSON.stringify(result4[0].score));
-                                          //var sql = "UPDATE cmf_user SET score ="+(result4 + newAmount)+" WHERE id = "+req.uid+";";
-                                          // con.query(sql, function (err, result3) {
-                                          //   if (err) throw err;
-                                          //     console.log("Result: " + result3);
-                                          //     client.query("INSERT INTO requests (request_type, sender_address, receiver_address, amount, datetime, uid, hash) VALUES (0, '"+sender+"', '"+receiver+"', "+amount+", NOW(), '"+hash+"')");
-                                          // });
+                                          var sql = "UPDATE cmf_user SET score ="+(JSON.stringify(result4[0].score) + newAmount)+" WHERE id = "+req.uid+";";
+                                          con.query(sql, function (err, result3) {
+                                            if (err) throw err;
+                                              console.log("Result: " + result3);
+                                              client.query("INSERT INTO requests (request_type, sender_address, receiver_address, amount, datetime, uid, hash) VALUES (0, '"+sender+"', '"+receiver+"', "+amount+", NOW(), '"+hash+"')");
+                                          });
                                         });
                                     });
                             });
                           }
-
-                  
-
-                        // con.query(sql, function (err, result3) {
-                        //   if (err) throw err;
-                        //     console.log("Result: " + result3);
-                        //     client.query("INSERT INTO requests (request_type, sender_address, receiver_address, amount, datetime, uid, hash) VALUES (0, '"+sender+"', '"+receiver+"', "+amount+", NOW(), '"+hash+"')");
-                        // });
-                                      //   }
-              //   else
-              //   {
-              //     res.send("错误：此订单未完成");
-              //   }
-              // }
-                });
-              }
-    });
+                  });
+                }
+                else
+                {
+                  res.send("错误：此订单未完成");
+                }
+  });
 });
 
 app.get('/contract/balance', async (req, res) => {
