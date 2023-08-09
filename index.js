2eab60adb4773056e95a12de68ad9924dbbaa3ba
Yamei-Github <headendekay2@outlook.com>
2023-08-09 16:59:01 +0800

    a

--- index.js
+++ index.js
@@ -476,10 +476,9 @@ ListenToContract();
 
 app.get('/test', async (req, res) => {
   
-  //let contract = await tronWeb.contract(abi).at(contractAddress);
   tronWeb.trx.getTransaction("3a875147518a55a1c57a114f630043fc8615b183cf43d2b2e82c83c1924b8c8d")
              .then(result => {
-              res.send(result.ret.contractRet);
+              res.send(result);
              });
 
 });
